d0c5b3157af979634eb81ef4589f01913b03d162
Michelle Salois Gadoury <e2078172@cmaisonneuve.qc.ca>
2023-04-21 09:44:37 -0400

    s12 modification du code du carrousel

--- js/carrousel.js
+++ js/carrousel.js
@@ -19,10 +19,10 @@
       carrousel.classList.remove('carrousel--activer')
    })
 
-   galerie.addEventListener('mousedown', function(){
+   /*galerie.addEventListener('mousedown', function(){
       carrousel.classList.add('carrousel--activer')
       ajouter_les_images_de_galerie()
-   })
+   })*/
 
 /**
  * Pour chaque image de la galerie l'ajouter dans le carrousel
@@ -35,6 +35,9 @@ function ajouter_les_images_de_galerie()
    for (const elem of galerie__img){
       ajouter_une_image_dans_carrousel(elem)
       ajouter_un_radio_bouton_dans_le_carrousel()
+      //écouteur
+      //la fonction doit exécuter une seul fois
+      //ajouter fonction affiche_image_carrousel
    }
 }
 
@@ -59,15 +62,24 @@ function ajouter_un_radio_bouton_dans_le_carrousel(){
    rad.dataset.index = position
    rad.addEventListener('mousedown', function(){
       index = this.dataset.index
-      if(ancienIndex != -1){
-         carrousel__figure.children[ancienIndex].style.opacity = "0"
-      }
-      //console.log(this.dataset.index)
-      carrousel__figure.children[index].style.opacity = "1"
-      ancienIndex = index
+      affiche_image_carrousel();
    })
    position = position + 1 //incrémentation de la position
    carrousel__form.appendChild(rad);
+}
+
+/**
+ * Affiche la nouvelle image du carrouel
+ */
+function affiche_image_carrousel(){
+   if(ancienIndex != -1){
+      carrousel__figure.children[ancienIndex].style.opacity = "0"
+      //carrousel__figure.children[ancienIndex].classList.remove('classebichoutruc--activer');
+   }
+   //console.log(this.dataset.index)
+   carrousel__figure.children[index].style.opacity = "1"
+   //carrousel__figure.children[index].classList.add('classebichoutruc--activer');
+   ancienIndex = index
 }
 
 
